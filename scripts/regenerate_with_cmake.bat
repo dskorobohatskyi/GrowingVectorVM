ff970e8b10d6c726f390ed6be6170b26c26de522
Dmytro <skorobogatskiidima@gmail.com>
2024-01-30 19:39:19 +0200

    Added automated script to regenerate solution

---
+++ scripts/regenerate_with_cmake.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set BUILD_DIR=build
+
+pushd ..
+if exist %BUILD_DIR% (
+    echo Removing CMakeCache in %BUILD_DIR%
+    del /q %BUILD_DIR%\CMakeCache.txt
+    del /q %BUILD_DIR%\cmake_install.cmake
+    rmdir /s /q %BUILD_DIR%\CMakeFiles
+    rem keep intermediate and binaries untouched for now
+) else (
+    mkdir %BUILD_DIR%
+    cd %BUILD_DIR%
+)
+pause
+
+echo Generating CMake solution in %BUILD_DIR%
+pushd %BUILD_DIR%
+cmake ..
+
+popd
+popd
+
+echo Done!
