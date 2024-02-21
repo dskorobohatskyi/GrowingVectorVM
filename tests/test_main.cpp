a06c5167341fa59757870662bee8fe270b2c6b9d
Dmytro Skorobohatskyi <skorobogatskiidima@gmail.com>
2024-02-22 01:01:45 +0200

    Included VectorsAdapter to build system. Implemented switching tests for std::vector and GrowingVectorVM

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,11 +1,25 @@
 #include "GrowingVectorVM.h"
 #include <gtest/gtest.h>
+#include "VectorsAdapter.h"
 
 
-TEST(GrowingArrayVMTests, EmptyTest)
+template <class T>
+class VectorTest : public testing::Test {
+
+protected:
+    constexpr static bool useStd = T::value;
+};
+
+typedef testing::Types<std::true_type, std::false_type> Variants;
+TYPED_TEST_SUITE(VectorTest, Variants);
+
+TYPED_TEST(VectorTest, VectorDefaultCtor)
 {
-    ASSERT_EQ(42, 42);
+    VectorAdapter<int, this->useStd> adapter;
+
+    ASSERT_TRUE(adapter.Empty());
 }
+
 
 int main(int argc, char** argv)
 {
