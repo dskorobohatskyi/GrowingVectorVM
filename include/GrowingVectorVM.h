a69452bee83440497675e66f164c4c3be2ec1fed
Dmytro Skorobohatskyi <skorobogatskiidima@gmail.com>
2024-03-01 01:31:39 +0200

    Carry out platform specific code to cpp

--- include/GrowingVectorVM.h
+++ include/GrowingVectorVM.h
@@ -1,16 +1,5 @@
 #pragma once
 
-// TODO Think about user experience with including your header-only library
-// Provide switching mechanism with static or dynamic library later.
-// Analyze which implemented functionality can be wrapped by free functions and be moved to cpp to reduce the compilation time.
-#ifdef WIN32
-#include <windows.h>
-#include <memoryapi.h>
-
-#include <sysinfoapi.h>                 // for GetPhysicallyInstalledSystemMemory
-#else
-#error Unsupported
-#endif
 
 #include <stdint.h>
 #include <assert.h>
@@ -31,8 +20,18 @@ static_assert(DS_MB(2) == 1'048'576 * 2);
 static_assert(DS_GB(4) == 4'294'967'296);
 
 
-// TODO make it a namespace or separate decl from implementation in the future
-struct ObjectLifecycleHelper
+#ifdef WIN32
+#ifdef GROWINGVECTORVM_EXPORTS
+#define GROWINGVECTORVM_API __declspec(dllexport)
+#else
+#define GROWINGVECTORVM_API __declspec(dllimport)
+#endif
+#else // GROWINGVECTORVM_STATIC
+#define GROWINGVECTORVM_API
+#endif
+
+
+namespace ObjectLifecycleHelper
 {
     template<typename T, typename... Args>
     static void ConstructObject(T* destination, Args&&... args)
@@ -53,143 +52,34 @@ struct ObjectLifecycleHelper
     }
 };
 
-// TODO make it a namespace or separate decl from implementation in the future (ideally should be moved entirely to cpp but will see)
-struct PlatformHelper
+namespace PlatformHelper
 {
-    [[nodiscard]] static size_t CalculateVirtualPageSize(const bool isLargePagesEnabled)
-    {
-        size_t pageSize = 0;
-        if (!isLargePagesEnabled)
-        {
-#if WIN32
-            SYSTEM_INFO sSysInfo;                   // Useful information about the system
-            GetSystemInfo(&sSysInfo);               // Initialize the structure.
+    [[nodiscard]] GROWINGVECTORVM_API size_t CalculateVirtualPageSize(const bool isLargePagesEnabled);
 
-            pageSize = sSysInfo.dwPageSize;         // Page size on this computer
-#else
-#error Not implemented
-#endif
-        }
-        else
-        {
-#if WIN32
-            pageSize = GetLargePageMinimum();
-#else
-#error Not implemented
-#endif
-        }
+    GROWINGVECTORVM_API void NullifyMemory(void* destination, const size_t size);
 
-        return pageSize;
-    }
+    [[nodiscard]] GROWINGVECTORVM_API size_t CalculateInstalledRAM();
 
-    static void NullifyMemory(void* destination, const size_t size)
-    {
-#if WIN32
-        memset(destination, 0, size);
-#else
-#error Not implemented
-#endif
-    }
-
-    [[nodiscard]] static size_t CalculateInstalledRAM()
-    {
-        unsigned long long TotalMemoryInBytes = 0;
-#if WIN32
-        GetPhysicallyInstalledSystemMemory(&TotalMemoryInBytes);
-        TotalMemoryInBytes *= 1024;
-#else
-#error Not implemented
-#endif
-        return TotalMemoryInBytes;
-    }
-
-    [[nodiscard]] static void* ReserveVirtualMemory(
+    [[nodiscard]] GROWINGVECTORVM_API void* ReserveVirtualMemory(
         const size_t alignedAllocationSize,
         const size_t pageSize,
         void* base = nullptr,
         const bool shouldCommitWithReserve = false,
         const bool isLargePagesEnabled = false
-    )
-    {
-        void* memory = nullptr;
-#if WIN32
-        DWORD allocationFlags = MEM_RESERVE;
-        DWORD protectionFlags = PAGE_NOACCESS;
-        if (shouldCommitWithReserve)
-        {
-            allocationFlags |= MEM_COMMIT;
-            protectionFlags = PAGE_READWRITE;
-        }
+    );
 
-        // Large-page memory must be reserved and committed as a single operation.
-        // In other words, large pages cannot be used to commit a previously reserved range of memory.
-        if (isLargePagesEnabled)
-        {
-            allocationFlags |= MEM_LARGE_PAGES;
-        }
-        // memory - Base address of the allocated memory
-        memory = VirtualAlloc(
-            base,                           // System selects address
-            alignedAllocationSize,          // Size of allocation
-            allocationFlags,                // Allocate reserved pages
-            protectionFlags);
-#else
-#error Not implemented
-#endif
-        return memory;
-    }
-
-    [[nodiscard]] static void* CommitVirtualMemory(
+    [[nodiscard]] GROWINGVECTORVM_API void* CommitVirtualMemory(
         void*& destination,
         const size_t memorySizeToCommit
         // TODO probably flags should be customizable also
-    )
-    {
-        void* committedMemory = nullptr;
-#if WIN32
-        committedMemory = VirtualAlloc(
-            destination,
-            memorySizeToCommit,
-            MEM_COMMIT,
-            PAGE_READWRITE);
-#else
-#error Not implemented
-#endif
-        return committedMemory;
-    }
+    );
 
-    static bool DecommitVirtualMemory(
+    bool GROWINGVECTORVM_API DecommitVirtualMemory(
         void*& destination,
         const size_t memorySizeToDecommit
-    )
-    {
-        bool result = false;
-#if WIN32
-        result = VirtualFree(
-            destination,
-            memorySizeToDecommit,
-            MEM_DECOMMIT);
-#else
-#error Not implemented
-#endif
-        return result;
-    }
+    );
 
-    static bool ReleaseVirtualMemory(
-        void* destination
-    )
-    {
-        bool result = false;
-#if WIN32
-        result = VirtualFree(
-            destination,                // Base address of block
-            0,                          // Bytes of committed pages, 0 for MEM_RELEASE, non-zero for DECOMMIT
-            MEM_RELEASE);               // Decommit the pages
-#else
-#error Not implemented
-#endif
-        return result;
-    }
+    bool GROWINGVECTORVM_API ReleaseVirtualMemory(void* destination);
 };
 
 
--- src/GrowingVectorVM.cpp
+++ src/GrowingVectorVM.cpp
@@ -1,1 +1,148 @@
 #include "GrowingVectorVM.h"
+
+#ifdef WIN32
+#include <windows.h>
+#include <memoryapi.h>
+
+#include <sysinfoapi.h>                 // for GetPhysicallyInstalledSystemMemory
+#else
+#error Unsupported
+#endif
+
+namespace PlatformHelper
+{
+    size_t CalculateVirtualPageSize(const bool isLargePagesEnabled)
+    {
+        size_t pageSize = 0;
+        if (!isLargePagesEnabled)
+        {
+#if WIN32
+            SYSTEM_INFO sSysInfo;                   // Useful information about the system
+            GetSystemInfo(&sSysInfo);               // Initialize the structure.
+
+            pageSize = sSysInfo.dwPageSize;         // Page size on this computer
+#else
+#error Not implemented
+#endif
+        }
+        else
+        {
+#if WIN32
+            pageSize = GetLargePageMinimum();
+#else
+#error Not implemented
+#endif
+        }
+
+        return pageSize;
+    }
+
+    void NullifyMemory(void* destination, const size_t size)
+    {
+#if WIN32
+        memset(destination, 0, size);
+#else
+#error Not implemented
+#endif
+    }
+
+    size_t CalculateInstalledRAM()
+    {
+        unsigned long long TotalMemoryInBytes = 0;
+#if WIN32
+        GetPhysicallyInstalledSystemMemory(&TotalMemoryInBytes);
+        TotalMemoryInBytes *= 1024;
+#else
+#error Not implemented
+#endif
+        return TotalMemoryInBytes;
+    }
+
+    void* ReserveVirtualMemory(
+        const size_t alignedAllocationSize,
+        const size_t pageSize,
+        void* base/* = nullptr*/,
+        const bool shouldCommitWithReserve/* = false*/,
+        const bool isLargePagesEnabled/* = false*/
+    )
+    {
+        void* memory = nullptr;
+#if WIN32
+        DWORD allocationFlags = MEM_RESERVE;
+        DWORD protectionFlags = PAGE_NOACCESS;
+        if (shouldCommitWithReserve)
+        {
+            allocationFlags |= MEM_COMMIT;
+            protectionFlags = PAGE_READWRITE;
+        }
+
+        // Large-page memory must be reserved and committed as a single operation.
+        // In other words, large pages cannot be used to commit a previously reserved range of memory.
+        if (isLargePagesEnabled)
+        {
+            allocationFlags |= MEM_LARGE_PAGES;
+        }
+        // memory - Base address of the allocated memory
+        memory = VirtualAlloc(
+            base,                           // System selects address
+            alignedAllocationSize,          // Size of allocation
+            allocationFlags,                // Allocate reserved pages
+            protectionFlags);
+#else
+#error Not implemented
+#endif
+        return memory;
+    }
+
+    void* CommitVirtualMemory(
+        void*& destination,
+        const size_t memorySizeToCommit
+        // TODO probably flags should be customizable also
+    )
+    {
+        void* committedMemory = nullptr;
+#if WIN32
+        committedMemory = VirtualAlloc(
+            destination,
+            memorySizeToCommit,
+            MEM_COMMIT,
+            PAGE_READWRITE);
+#else
+#error Not implemented
+#endif
+        return committedMemory;
+    }
+
+    bool DecommitVirtualMemory(
+        void*& destination,
+        const size_t memorySizeToDecommit
+    )
+    {
+        bool result = false;
+#if WIN32
+        result = VirtualFree(
+            destination,
+            memorySizeToDecommit,
+            MEM_DECOMMIT);
+#else
+#error Not implemented
+#endif
+        return result;
+    }
+
+    bool ReleaseVirtualMemory(
+        void* destination
+    )
+    {
+        bool result = false;
+#if WIN32
+        result = VirtualFree(
+            destination,                // Base address of block
+            0,                          // Bytes of committed pages, 0 for MEM_RELEASE, non-zero for DECOMMIT
+            MEM_RELEASE);               // Decommit the pages
+#else
+#error Not implemented
+#endif
+        return result;
+    }
+};
